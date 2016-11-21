5fc9e58b4ef16a96b55981c1ea3f6d19a0661291
Nicolas BREMONT <bremont.nicolas@gmail.com>
2016-11-21 17:24:22 +0100

    [master] style: Fix typo

--- src/Cp/DomainObject/TypeInterface.php
+++ src/Cp/DomainObject/TypeInterface.php
@@ -9,5 +9,5 @@ interface TypeInterface
 {
     const TYPE_10K = 'plan-entrainement-10km';
     const TYPE_SEMI = 'plan-entrainement-semi-marathon';
-    const TYPE_MARATHON = ' plan-entrainement-marathon';
+    const TYPE_MARATHON = 'plan-entrainement-marathon';
 }
